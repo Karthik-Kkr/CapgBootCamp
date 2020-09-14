829c2d099338188a20003396fa57feca3e36eb55
karth <karth@192.168.2.8>
2020-09-14 22:26:21 +0530

    New things added

---
+++ Selenium/src/test/java/com/seleniumlearning/HeadLessBrowser.java
@@ -0,0 +1,18 @@
+package com.seleniumlearning;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class HeadLessBrowser {
+	public static void main(String[] args) {
+		
+//		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new HtmlUnitDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		System.out.println(driver.getTitle());
+	}
+
+}
