efe9e5fefd3ae46e6add7ab98aa4842db2300e92
karth <karth@192.168.2.8>
2020-09-14 16:10:18 +0530

    Selenium concepts

---
+++ Selenium/src/test/java/com/seleniumlearning/OrangeHrmLogin.java
@@ -0,0 +1,26 @@
+package com.seleniumlearning;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OrangeHrmLogin {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://www.facebook.com/");
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		System.out.println("the title of page is:" + driver.getTitle());
+		
+//		WebDriver driver=new ChromeDriver();
+//		driver.get("https://opensource-demo.orangehrmlive.com/");
+
+	}
+}
